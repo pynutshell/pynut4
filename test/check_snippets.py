8627c292320f0374cbfdaec93fe70a4663ccc52a
ptmcg <ptmcg@austin.rr.com>
2022-01-26 08:40:49 -0600

    Simple snippet-checker

---
+++ test/check_snippets.py
@@ -0,0 +1,30 @@
+#
+# check_snippets.py
+#
+
+from pathlib import Path
+
+test_dir = Path(__file__).parent
+snippets = test_dir.parent.rglob("*.py")
+
+# do they compile?
+total = 0
+fail_snips = []
+for snip in snippets:
+    total += 1
+    try:
+        compile(snip.read_text(), str(snip), "exec")
+    except Exception as exc:
+        print(f"Failed to compile {snip}")
+        print(exc)
+        fail_snips.append((snip, str(exc)))
+    else:
+        print(f"Successfully compiled {snip}")
+
+print()
+if fail_snips:
+    print(f"{len(fail_snips)} of {total} files failed to compile:")
+    for snip, msg in fail_snips:
+        print(f"- {snip}\n    {msg}")
+else:
+    print(f"All {total} .py files compiled")
