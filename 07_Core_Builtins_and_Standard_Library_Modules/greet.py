56ee4b7a3df58d82e73c36a0054295f96fb61e34
Paul McGuire <ptmcg@users.noreply.github.com>
2022-01-26 23:01:15 -0600

    Ch7 example greet.py

---
+++ 07_Core_Builtins_and_Standard_Library_Modules/greet.py
@@ -0,0 +1,13 @@
+import argparse
+
+ap = argparse.ArgumentParser(description='Just an example')
+ap.add_argument('who', nargs='?', default='World')
+ap.add_argument('--formal', action='store_true')
+
+ns = ap.parse_args()
+if ns.formal:
+    greet = 'Most felicitous salutations, o {}.'
+else:
+    greet = 'Hello, {}!'
+
+print(greet.format(ns.who))
