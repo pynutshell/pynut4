53403a97a889268881a955149f73600e38538298
ptmcg <ptmcg@austin.rr.com>
2022-01-27 07:45:34 -0600

    Cleaner filename logging in check_snippets.py; exit with nonzero return code if any fails found

--- test/check_snippets.py
+++ test/check_snippets.py
@@ -3,6 +3,7 @@
 #
 
 from pathlib import Path
+import sys
 
 test_dir = Path(__file__).parent
 project_dir = test_dir.parent
@@ -12,15 +13,16 @@ snippets = project_dir.rglob("*.py")
 total = 0
 fail_snips = []
 for snip in snippets:
+    log_snip = str(snip.relative_to(project_dir))
     total += 1
     try:
         compile(snip.read_text(), str(snip), "exec")
     except Exception as exc:
-        print(f"Failed to compile {snip}")
+        print(f"Failed to compile {log_snip}")
         print(exc)
         fail_snips.append((snip, str(exc)))
     else:
-        print(f"Successfully compiled {snip}")
+        print(f"Successfully compiled {log_snip}")
 
 print()
 if fail_snips:
@@ -29,3 +31,5 @@ if fail_snips:
         print(f"- {snip}\n    {msg}")
 else:
     print(f"All {total} .py files compiled")
+
+sys.exit(0 if not fail_snips else 1)
