87c0d4913f2fbd82c1efc8978bbf2d1420f5b358
ptmcg <ptmcg@austin.rr.com>
2022-02-08 02:09:05 -0600

    Add __slots__ with dict example

--- 04_Object_Oriented_Python/slots_rectangle.py
+++ 04_Object_Oriented_Python/slots_rectangle.py
@@ -10,6 +10,15 @@ class Rectangle(object):
 class OptimizedRectangle(Rectangle):
     __slots__ = 'width', 'height'
 
+# 3.8++
+class OptimizedRectangle(Rectangle):
+    __slots__ = {'width': 'rectangle width in pixels',
+                 'height': 'rectangle height in pixels'}
+
+# display docstrings with help()
+help(OptimizedRectangle)
+
+
 opt_r = OptimizedRectangle(100, 100)
 print(vars(opt_r))  # <- prints empty dict, since __slots__ objects have no __dict__
 print({name: getattr(opt_r, name) for name in opt_r.__slots__})
