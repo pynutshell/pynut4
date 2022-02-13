debe78e4c235aefa76a4f0666a9a89f6b89f297f
ptmcg <ptmcg@austin.rr.com>
2022-02-12 18:49:24 -0600

    Add CSV example (using single quotes)

--- 11_Persistence_and_Databases/csv_colors.py
+++ 11_Persistence_and_Databases/csv_colors.py
@@ -1,6 +1,6 @@
 import csv
 
-color_data = """\
+color_data = '''\
 color,r,g,b
 red,255,0,0
 green,0,255,0
@@ -8,9 +8,9 @@ blue,0,0,255
 cyan,0,255,255
 magenta,255,0,255
 yellow,255,255,0
-""".splitlines()
+'''.splitlines()
 
-colors = {row["color"]: row
+colors = {row['color']: row
           for row in csv.DictReader(color_data)}
 
-print(colors["red"])  # prints {'color': 'red', 'r': '255', 'g': '0', 'b': '0'}
+print(colors['red'])  # prints {'color': 'red', 'r': '255', 'g': '0', 'b': '0'}
