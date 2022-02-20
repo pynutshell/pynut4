8982c4a84d1b546d0698b886e1a975cb01c7e6e0
ptmcg <ptmcg@austin.rr.com>
2022-02-20 08:03:28 -0600

    Add helpful comments to pdb_example.py

--- 16_Testing_Debugging_and_Optimizing/pdb_example.py
+++ 16_Testing_Debugging_and_Optimizing/pdb_example.py
@@ -2,10 +2,20 @@ import pdb
 
 def f(x):
     pdb.set_trace()
+    # At (Pdb) prompt, 'x' will display the value for x
+
+    # Enter 'c' command to advance to the next breakpoint
     if x > 100:
         q = x**2
     else:
         q = x
+
+    pdb.set_trace()
+    # At (Pdb) prompt, 'q' will *not* display the value for q
+    # since 'q' is defined as an abbreviation for the Pdb 'quit'
+    # command - exiting the debugger and the program!
+    #
+    # To view the value for q, enter '!q' or `p q`.
     return q
 
 f(10)
