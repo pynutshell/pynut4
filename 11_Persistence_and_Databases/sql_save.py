b88b79ed208cfdd024a713b8671275ccaca544e7
ptmcg <ptmcg@austin.rr.com>
2022-02-20 07:44:44 -0600

    Update to use Connection as context manager

--- 11_Persistence_and_Databases/sql_save.py
+++ 11_Persistence_and_Databases/sql_save.py
@@ -1,11 +1,11 @@
 import fileinput, sqlite3
 connect = sqlite3.connect('database.db')
 cursor = connect.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS Words '
-               '(Word TEXT, File TEXT, Line INT)')
-for line in fileinput.input():
-    f, l = fileinput.filename(), fileinput.filelineno()
-    cursor.executemany('INSERT INTO Words VALUES (:w, :f, :l)',
-        [{'w':w, 'f':f, 'l':l} for w in line.split()])
-connect.commit()
+with connect:
+    cursor.execute('CREATE TABLE IF NOT EXISTS Words '
+                   '(Word TEXT, File TEXT, Line INT)')
+    for line in fileinput.input():
+        f, l = fileinput.filename(), fileinput.filelineno()
+        cursor.executemany('INSERT INTO Words VALUES (:w, :f, :l)',
+            [{'w':w, 'f':f, 'l':l} for w in line.split()])
 connect.close()
