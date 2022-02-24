ecae45ba3ad8da6bf63773f6530feeb5f9be009b
ptmcg <ptmcg@austin.rr.com>
2022-02-24 16:27:42 -0600

    Add memoizing.py example.

---
+++ 16_Testing_Debugging_and_Optimizing/memoizing.py
@@ -0,0 +1,27 @@
+import math
+def sin_degrees(x):
+    return math.sin(math.radians(x))
+
+# function with unbounded memoizing cache
+cached_values = {}
+def sin_degrees(x):
+    if x not in cached_values:
+        cached_values[x] = math.sin(math.radians(x))
+    return cached_values[x]
+
+# function with bounded FIFO memoizing cache
+cached_values = {}
+maxsize = 20
+def sin_degrees(x):
+    if x not in cached_values:
+        cached_values[x] = math.sin(math.radians(x))
+        if len(cached_values) > maxsize:
+            oldest_key = next(iter(cached_values))
+            del cached_values[oldest_key]
+    return cached_values[x]
+
+# function with bounded LRU cache
+import functools
+@functools.lru_cache(maxsize=20)
+def sin_degrees(x):
+    return math.sin(math.radians(x))
