637b1232acc1f89a36617b94c865d01a74d55b0a
ptmcg <ptmcg@austin.rr.com>
2022-05-09 04:37:22 -0500

    Illustrate using add_note() and accessing __notes__ attribute safely

--- 05_Exceptions/exception_add_note.py
+++ 05_Exceptions/exception_add_note.py
@@ -1,5 +1,20 @@
 try:
-    1/0
-except ZeroDivisionError as zde:
-    zde.add_note("This was intentional")
+    try:
+        # intentional div-by-zero (for demonstration)
+        1 / 0
+
+        # unintentional TypeError (comment out previous line to see change in exception handling)
+        1 / "0"
+
+    except ZeroDivisionError as zde:
+        zde.add_note("This was intentional")
+        raise
+    except Exception as e:
+        e.add_note("This wasn't intentional")
+        raise
+
+except Exception as e:
+    # flag unintentional exceptions for attention
+    if "This was intentional" not in getattr(e, "__notes__", []):
+        e.add_note("Needs attention!!!")
     raise
