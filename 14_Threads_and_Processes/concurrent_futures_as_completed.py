790a23e3e4a92f38120f66b4e948ebef92d78169
ptmcg <ptmcg@austin.rr.com>
2022-05-29 08:27:04 -0500

    Redo concurrent_futures_as_completed.py to deobfuscate nested dict() construction (break out creation of futures in separate statement)

--- 14_Threads_and_Processes/concurrent_futures_as_completed.py
+++ 14_Threads_and_Processes/concurrent_futures_as_completed.py
@@ -4,9 +4,9 @@ from concurrent_futures import runner
 
 def make_dict(strings):
     with cf.ProcessPoolExecutor() as e:
+        futures = [e.submit(runner, s) for s in strings]
         d = dict(f.result()
-                 for f in cf.as_completed(
-                    e.submit(runner, s) for s in strings))
+                 for f in cf.as_completed(futures))
     return d
 
 if __name__ == '__main__':
