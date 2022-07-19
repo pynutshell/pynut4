cd5e8fb6dea66aa00af7e11873c89975f2dbd4ad
ptmcg <ptmcg@austin.rr.com>
2022-07-18 20:15:34 -0500

    Fix exception handling form in doctest

--- 08_Strings_and_Things/unicodedata_module.py
+++ 08_Strings_and_Things/unicodedata_module.py
@@ -1,17 +1,18 @@
-'''
+"""
 >>> import unicodedata
 >>> unicodedata.name("🌈")
 'RAINBOW'
 >>> unicodedata.name("Ⅵ")
 'ROMAN NUMERAL SIX'
 >>> int("Ⅵ")
+Traceback (most recent call last):
+  ...
 ValueError: invalid literal for int() with base 10: 'Ⅵ'
 >>> unicodedata.numeric("Ⅵ")  # use unicodedata to get the numeric value
 6.0
 >>> unicodedata.lookup("MUSICAL SCORE")
 '🎼'
-'''
-
+"""
 
 if __name__ == '__main__':
     # use doctest to simulate console sessions
