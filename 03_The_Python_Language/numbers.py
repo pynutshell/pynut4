b51edf6c9e4fe19a8ab10af9c0607699e91d0d51
ptmcg <ptmcg@austin.rr.com>
2022-07-20 08:42:40 -0500

    Reviewer feedback

--- 03_The_Python_Language/numbers.py
+++ 03_The_Python_Language/numbers.py
@@ -1,7 +1,7 @@
-1, 23, 3493                  # Decimal integer literals
-0b010101, 0b110010           # Binary integer literals
-0o1, 0o27, 0o6645            # Octal integer literals
-0x1, 0x17, 0xDA5, 0xda5      # Hexadecimal integer literals
+1, 23, 3493                    # Decimal integer literals
+0b010101, 0b110010, 0B01       # Binary integer literals
+0o1, 0o27, 0o6645, 0O777       # Octal integer literals
+0x1, 0x17, 0xDA5, 0xda5, 0Xff  # Hexadecimal integer literals
 
 0., 0.0, .0, 1., 1.0, 1e0, 1.e0, 1.0e0   # Floating-point literals
 
