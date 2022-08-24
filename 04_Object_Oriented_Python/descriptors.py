9cc69eddca30e0968021dc5989916364470a65c2
ptmcg <ptmcg@austin.rr.com>
2022-08-24 08:36:44 -0500

    Add __delete__ method to descriptors example

--- 04_Object_Oriented_Python/descriptors.py
+++ 04_Object_Oriented_Python/descriptors.py
@@ -8,6 +8,9 @@ class Const:  # an overriding descriptor, see later
     def __get__(self, *_):  # always return the constant value
         return self.value
 
+    def __delete__(self, *_):  # const cannot be deleted
+        pass
+
 
 class X:
     c = Const(23)
@@ -17,3 +20,6 @@ x = X()
 print(x.c)  # prints: 23
 x.c = 42  # silently ignored
 print(x.c)  # prints: 23
+del x.c
+print(x.c)  # prints: 23
+
