6b44531f6dd2ec6081d1ce01b0cc2597470889e7
ptmcg <ptmcg@austin.rr.com>
2022-08-24 10:47:45 -0500

    Update IPv6 copies of network client/server examples to mirror IPv4 versions

--- 18_Basic_Networking/tcpserver6.py
+++ 18_Basic_Networking/tcpserver6.py
@@ -1,27 +1,30 @@
 from concurrent import futures as cf
 import socket
- 
+
+IP_ADDR = 'localhost'
+IP_PORT = 8881
+
 def handle(new_sock, address):
     print('Connected from', address)
-    while True:
-        received = new_sock.recv(1024)
-        if not received: break
-        s = received.decode('utf-8', errors='replace')
-        print('Recv:', s)
-        new_sock.sendall(received)
-        print('Echo:', s)
-    new_sock.close()
-    print('Disconnected from', address)
-servsock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-servsock.bind(('localhost', 8881))
-servsock.listen(5)
-print('Serving at', servsock.getsockname())
-with cf.ThreadPoolExecutor(20) as e:
-    try:
+    with new_sock:
+        while True:
+            received = new_sock.recv(1024)
+            if not received:
+                break
+            s = received.decode('utf-8', errors='replace')
+            print(f'Recv: {s!r}')
+            new_sock.sendall(received)
+            print(f'Echo: {s!r}')
+    print(f'Disconnected from {address}')
+
+
+with socket.socket(socket.AF_INET6,    # IPv6
+                   socket.SOCK_STREAM  # TCP
+                   ) as servsock:
+    servsock.bind((IP_ADDR, IP_PORT))
+    servsock.listen(5)
+    print(f'Serving at {servsock.getsockname()}')
+    with cf.ThreadPoolExecutor(20) as e:
         while True:
             new_sock, address = servsock.accept()
             e.submit(handle, new_sock, address)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        servsock.close()
