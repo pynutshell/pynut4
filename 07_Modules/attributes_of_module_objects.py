3721905c3e7e1d7f6c14f99242156a547786db5c
ptmcg <ptmcg@austin.rr.com>
2022-08-24 08:36:05 -0500

    Update attributes_of_module_objects.py to illustrate use of module level __getattr__

--- 07_Modules/attributes_of_module_objects.py
+++ 07_Modules/attributes_of_module_objects.py
@@ -1,3 +1,5 @@
+import sys
+
 import mymodule
 a = mymodule.f()
 
@@ -5,3 +7,18 @@ a = mymodule.f()
 import mymodule as alias
 a = alias.f()
 
+
+# get dynamically generated attribute
+print(f'10,000th prime is {mymodule.first_10000_primes[-1]}')
+
+def find_prime_triples():
+    for a, b, c in zip(mymodule.first_10000_primes,
+                       mymodule.first_10000_primes[1:],
+                       mymodule.first_10000_primes[2:]):
+        if c-a == 6:
+            yield a, b, c
+
+for x in find_prime_triples():
+    print(x)
+
+print(dir(mymodule))
--- 07_Modules/mymodule.py
+++ 07_Modules/mymodule.py
@@ -1,4 +1,27 @@
-# sample module, to be used to illustrate importing a module
+# sample module, to be used to illustrate importing a module, and module-level __getattr__
+from collections.abc import MutableSequence
 
 def f():
     return None
+
+
+def __getattr__(name):
+    if name == 'first_10000_primes':
+        import sys
+        print(f"initializing {name}")
+        # get current module object by looking up __name__ in sys.modules
+        this_module = sys.modules[__name__]
+
+        def generate_n_primes(n):
+            found = [2]
+            i = 3
+            while len(found) < n:
+                if not any(i % x == 0 for x in found):
+                    found.append(i)
+                i += 2
+            return found
+
+        this_module.first_10000_primes = generate_n_primes(10000)
+        return this_module.first_10000_primes
+
+    raise AttributeError(f'no such attribute {name!r}')
