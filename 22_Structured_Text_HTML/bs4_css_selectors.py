58a43fb15ac3a1467b6f0ce64529576819bf37d6
ptmcg <ptmcg@austin.rr.com>
2022-09-10 06:58:02 -0500

    Fix CSS selector; add exec'er to run examples

--- 22_Structured_Text_HTML/bs4_css_selectors.py
+++ 22_Structured_Text_HTML/bs4_css_selectors.py
@@ -5,8 +5,22 @@ def foo_child_of_bar(t):
 
 soup = bs4.BeautifulSoup('<bar>Plain <foo>bold</foo></bar>')
 
-soup(foo_child_of_bar)
+soup.find_all(foo_child_of_bar)
 # returns tags with name 'foo' children of tags with name 'bar'
 
-soup.select('foo < bar')
+soup.select('bar > foo')
 # exactly equivalent, with no custom filter function needed
+
+
+###
+# code to execute the above search statements and print the results of each
+with open(__file__) as source:
+    for line in source:
+        line = line.rstrip()
+        if line == "###":
+            break
+
+        print(line)
+        if line.startswith("soup."):
+            statement = line.partition("#")[0]
+            exec(f"print({statement})", globals())
