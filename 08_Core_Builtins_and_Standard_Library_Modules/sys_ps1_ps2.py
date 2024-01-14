397290a612450e8a23521abd367390389bb109d1
ptmcg <ptmcg@austin.rr.com>
2024-01-13 18:42:31 -0600

    Errata https://app.oreilly.com/cs/portal/edit/errata/200834 - code typo, legacy inheritance from object, not required in Python 3

--- 08_Core_Builtins_and_Standard_Library_Modules/sys_ps1_ps2.py
+++ 08_Core_Builtins_and_Standard_Library_Modules/sys_ps1_ps2.py
@@ -1,14 +1,14 @@
 import sys
 
 
-class Ps1(object):
+class Ps1:
     def __init__(self):
         self.p = 0
     def __str__(self):
         self.p += 1
         return f'[{self.p}]>>> '
 
-class Ps2(object):
+class Ps2:
     def __str__(self):
         return f'[{sys.ps1.p}]... '
 
