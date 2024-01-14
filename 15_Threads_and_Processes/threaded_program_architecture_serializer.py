d036dc2d2bf53ed07dc84a2c52519f58cd1bc5de
ptmcg <ptmcg@austin.rr.com>
2024-01-13 18:22:16 -0600

    Errata https://app.oreilly.com/cs/portal/edit/errata/200586 - address threading bug cited in erratum; use results dict instead of queue to keep results aligned with requests from different threads

--- 15_Threads_and_Processes/threaded_program_architecture_serializer.py
+++ 15_Threads_and_Processes/threaded_program_architecture_serializer.py
@@ -1,4 +1,7 @@
-import threading, queue
+import queue
+import threading
+import time
+from itertools import count
 
 
 class Serializer(threading.Thread):
@@ -6,15 +9,45 @@ class Serializer(threading.Thread):
         super().__init__(**kwds)
         self.daemon = True
         self.work_request_queue = queue.Queue()
-        self.result_queue = queue.Queue()
+        self.work_results = {}
+        self.request_id_generator = count()
         self.start()
-
-    def apply(self, callable, *args, **kwds):
-        """called by other threads as `callable` would be"""
-        self.work_request_queue.put((callable, args, kwds))
-        return self.result_queue.get()
 
     def run(self):
         while True:
-            callable, args, kwds = self.work_request_queue.get()
-            self.result_queue.put(callable(*args, **kwds))
+            seq_num, callable, args, kwds = self.work_request_queue.get(timeout=3)
+            self.work_results[seq_num] = callable(*args, **kwds)
+
+    def apply(self, callable, *args, **kwds):
+        """called by other threads as `callable` would be"""
+        request_seq_num = next(self.request_id_generator)
+        self.work_request_queue.put((request_seq_num, callable, args, kwds))
+        result_not_yet_available_sentinel = object()
+        while True:
+            result = self.work_results.pop(request_seq_num, result_not_yet_available_sentinel)
+            if result is not result_not_yet_available_sentinel:
+                return result
+            time.sleep(0.05)
+
+
+# ====== demo only - do not include in book copy ======
+
+if __name__ == '__main__':
+
+    """Tests the Serializer class."""
+    serializer = Serializer()
+
+    def request_fn(ii):
+        expected = ii*ii
+        actual = serializer.apply(lambda x: x * x, ii)
+        if True:  # actual != expected:
+            print(
+                f"\n{expected=}, {actual=} {'<<<' if (expected != actual) else ''}",
+                end="",
+                flush=True
+            )
+
+    num_threads = 100
+    for i in range(num_threads):
+        thread = threading.Thread(target=request_fn, args=(i,))
+        thread.start()
